3644c0df173e834f935b850b713280b0a8264e00
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-08-04 13:09:32 -0300

    convert the data to be passed and add a user to the database

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-fetch('https://reqres.in/api/users', {
+fetch('https://reqres.in/api/users', { 
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'  //telling Fetch that you will gonna be passing JSON
     },
+    body: JSON.stringify({ //converting the object into JSON to be transported, 
+                           //the body is where the content you want to add go
+        id: 7,
+        email: 'bob.prescott@reqres.in',
+        first_name: 'Robert',
+        last_name: 'Prescott',
+        avatar: 'https://reqres.in/img/faces/10-image.jpg' //adding a new user into the database
+    })
     
 })
