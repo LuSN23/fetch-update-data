a4a113af217f7d637dffcea2d92ed7f458dbe04b
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-08-04 12:36:01 -0300

    add headers to inform fetch about the type of the content that will be passed ahead

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 fetch('https://reqres.in/api/users', {
     method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'  //telling Fetch that you will gonna be passing JSON
+    },
     
 })
