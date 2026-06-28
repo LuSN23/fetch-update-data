fc3e012b0c2af7888fc63f825f584b2341c608f0
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2026-06-27 21:40:54 -0300

    chore: pequena ajuste nos comentários

--- script.mjs
+++ script.mjs
@@ -1,18 +1,18 @@
 import fetch from 'node-fetch';
 
-fetch('https://reqres.in/api/users', {  //applying a url to fetch data through an fake API called reqres
+fetch('https://reqres.in/api/users', {  // aplicando uma URL para buscar dados através de uma API fake chamada reqres
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json'  //telling Fetch that you will gonna be passing JSON
+        'Content-Type': 'application/json'  // informando ao Fetch que você vai passar JSON
     },
-    body: JSON.stringify({ //converting the object into JSON to be transported, 
-                           //the body is where the content you want to add go
+    body: JSON.stringify({ // convertendo o objeto em JSON para ser transportado,
+                           // o body é onde vai o conteúdo que você quer adicionar
         id: 7,
         email: 'bob.prescott@reqres.in',
         first_name: 'Robert',
         last_name: 'Prescott',
-        avatar: 'https://reqres.in/img/faces/10-image.jpg' //adding a new user into the database
+        avatar: 'https://reqres.in/img/faces/10-image.jpg' // adicionando um novo usuário ao banco de dados
     })
-}).then(response => response.json()) //receveing the Response object from Fetch, converting the data to JSON and passing ahead to the other .then
-  .then(data => console.log(data))   //receveing the JSON converted response(Promise) and showing it in the log
-  .catch(error => console.log('ERROR', error.message)); //handling possible errors
+}).then(response => response.json()) // recebendo o objeto Response do Fetch, convertendo os dados para JSON e passando adiante para o próximo .then
+  .then(data => console.log(data))    // recebendo a resposta convertida em JSON (Promise) e mostrando no log
+  .catch(error => console.log('ERROR', error.message)); // tratando possíveis erros
