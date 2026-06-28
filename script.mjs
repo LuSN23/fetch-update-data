9aaf6d195cb63cc07d494ce4586062b34278785e
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2026-06-27 23:39:15 -0300

    Adiciona método GET para consumir dados da API ReqRes

--- script.mjs
+++ script.mjs
@@ -1,20 +1,37 @@
+//Para GET - Recuperar usuários/dados - uma chave grátis somente leitura basta
 import 'dotenv/config';
 import fetch from 'node-fetch';
 
-fetch('https://reqres.in/api/users', {  // aplicando uma URL para buscar dados através de uma API fake chamada reqres
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',  // informando ao Fetch que você vai passar JSON
-        'x-api-key': process.env.API_KEY_TEST    // pegando a chave do arquivo .env por questão de segurança
-    },
-    body: JSON.stringify({ // convertendo o objeto em JSON para ser transportado,
-                           // o body é onde vai o conteúdo que você quer adicionar
-        id: 7,
-        email: 'bob.prescott@reqres.in',
-        first_name: 'Robert',
-        last_name: 'Prescott',
-        avatar: 'https://reqres.in/img/faces/10-image.jpg' // adicionando um novo usuário ao banco de dados
-    })
-}).then(response => response.json()) // recebendo o objeto Response do Fetch, convertendo os dados para JSON e passando adiante para o próximo .then
-  .then(data => console.log(data))    // recebendo a resposta convertida em JSON (Promise) e mostrando no log
-  .catch(error => console.log('ERROR', error.message)); // tratando possíveis erros
+fetch('https://reqres.in/api/users?page=2', {
+  method: 'GET',                            //método para puxar do servidor os usuários
+  headers: {
+    'Content-Type': 'application/json',
+    'x-api-key': process.env.API_KEY_TEST   //uso de uma public key
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log('ERROR', error.message));
+
+
+//Para POST - Adicionar usuários/dados - necessita de uma chave de API paga
+// import 'dotenv/config';
+// import fetch from 'node-fetch';
+
+// fetch('https://reqres.in/api/users', {  // aplicando uma URL para buscar dados através de uma API fake chamada reqres
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',  // informando ao Fetch que você vai passar JSON
+//         'x-api-key': process.env.API_KEY_TEST    // pegando a chave do arquivo .env por questão de segurança
+//     },
+//     body: JSON.stringify({ // convertendo o objeto em JSON para ser transportado,
+//                            // o body é onde vai o conteúdo que você quer adicionar
+//         id: 7,
+//         email: 'bob.prescott@reqres.in',
+//         first_name: 'Robert',
+//         last_name: 'Prescott',
+//         avatar: 'https://reqres.in/img/faces/10-image.jpg' // adicionando um novo usuário ao banco de dados
+//     })
+// }).then(response => response.json()) // recebendo o objeto Response do Fetch, convertendo os dados para JSON e passando adiante para o próximo .then
+//   .then(data => console.log(data))    // recebendo a resposta convertida em JSON (Promise) e mostrando no log
+//   .catch(error => console.log('ERROR', error.message)); // tratando possíveis erros
